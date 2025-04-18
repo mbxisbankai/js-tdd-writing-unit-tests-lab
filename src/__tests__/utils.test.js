984994e944c877e26f685766bb771a14e2e6086f
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-04-18 16:26:23 +0300

    TDD Writing Unit Tests Lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import isPalindrome from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true if word is a palindrome, otherwise it returns false", () => {
+        const word1 = "racecar";
+        const word2 = "car";
+
+        const reversedword1 = isPalindrome(word1);
+        const reversedword2 = isPalindrome(word2);
+
+        expect(reversedword1).toBe(true);
+        expect(reversedword2).toBe(false);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+function isPalindrome(word){
+    return word === word.split("").reverse().join("") ? true : false;
+}
+
+export default isPalindrome;
